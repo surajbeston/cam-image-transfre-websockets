02695c118bc6e6badf2b7300d631ac84b819e7fe
Suraj Jha <33459713+surajbeston@users.noreply.github.com>
2020-01-22 12:39:41 +0545

    Commit fourth

--- client.py
+++ client.py
@@ -4,27 +4,52 @@ from PIL import Image
 
 # import the base64 module
 import base64
-
+from datetime import datetime
+import numpy as np
+import cv2
+import codecs
+import time
 
 
 class MyClientProtocol(WebSocketClientProtocol):
-   result_file = "1"
+
 
    def onConnect(self, response):
       print("Server connected: {0}".format(response.peer))
 
    def onOpen(self):
       print("WebSocket connection open.")
-      self.sendMessage(bytes.fromhex("7342"))
+      message = "Connection opened....".encode()
+      message = codecs.encode(message, "hex")
+      self.sendMessage(message)
 
 
    def onMessage(self, payload, isBinary):
       if isBinary:
-         self.result_file = self.result_file + "1"
+         face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+         eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
+         result_file = str(datetime.now())
          print("Binary message received: {0} bytes".format(len(payload)))
-         with open(self.result_file+".png", 'wb') as file:
+         with open("img/" + result_file+".jpg", 'wb') as file:
               file.write(payload)
-         self.sendMessage(bytes.fromhex("7342"))
+
+
+         img = cv2.imread("img/"+result_file + ".jpg")
+         print (img)
+         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+         print (gray)
+         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+         print (faces)
+         if len(faces) == 4:
+                  print (faces[0])
+                  message = "Face Detected".encode()
+                  message = codecs.encode(message, "hex")
+                  self.sendMessage(message)
+         else:
+                  message = "Face Not Detected".encode()
+                  message = codecs.encode(message, "hex")
+                  self.sendMessage(message)
       else:
          # printing the size of the encoded image which is received
          print("Encoded size of the received image: {0} bytes".format(len(payload)))
@@ -44,8 +69,8 @@ if __name__ == '__main__':
 
    log.startLogging(sys.stdout)
 
-   factory = WebSocketClientFactory("ws://localhost:9000")
+   factory = WebSocketClientFactory("ws://192.168.2.2:9000")
    factory.protocol = MyClientProtocol
 
-   reactor.connectTCP("127.0.0.1", 9000, factory)
+   reactor.connectTCP("192.168.2.2", 9000, factory)
    reactor.run()
